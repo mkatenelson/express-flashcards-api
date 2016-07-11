2f654925b2df06545b04cdafc484c035e2e06f40
Kate Nelson <mkatenelson@gmail.com>
2016-07-11 14:18:00 -0700

    setup

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.connect( process.env.MONGOLAB_COBALT_URI || "mongodb://0.0.0.0/flashcards");
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://heroku_lbq5hw80:eihv2qanblep17hp5rujpat66h@ds021289.mlab.com:21289/heroku_lbq5hw80");
 
 // After creating a new model, require and export it:
 module.exports.Flashcard = require("./flashcard.js");
--- server.js
+++ server.js
@@ -49,3 +49,4 @@ app.listen(app.get("port"), function() {
     });
   console.log("Express server listening on port %d in %s mode", app.get("port"), app.settings.env);
 });
+app.listen(process.env.PORT || 5000)
